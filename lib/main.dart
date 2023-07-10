d84dab9f86d483bb5d1071efa613949917f949eb
emilkaagni <basnyatemil02@gmail.com>
2023-07-10 11:47:52 +0545

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:Indi_shark/views/splash_screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'consts/consts.dart';
 
 void main() {
+// async{
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
