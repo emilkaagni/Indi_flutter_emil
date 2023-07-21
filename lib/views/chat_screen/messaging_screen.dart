583bac057beddd0f699451f356b66bbb0256a37b
emilkaagni <basnyatemil02@gmail.com>
2023-07-21 22:46:11 +0545

    created a my message page with it pulling messages from the firebase

--- lib/views/chat_screen/messaging_screen.dart
+++ lib/views/chat_screen/messaging_screen.dart
@@ -25,7 +25,34 @@ class MessagesScreen extends StatelessWidget {
           } else if(snapshot.data!.docs.isEmpty){
             return "No messages yet!".text.color(darkFontGrey).makeCentered();
             }else {
-            return Container();
+            var data = snapshot.data!.docs;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Expanded(child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (BuildContext context, int index){
+                        return Card(
+                          child: ListTile(
+                            leading: const CircleAvatar(
+                              backgroundColor: redColor,
+                              child: Icon(
+                                Icons.person,
+                                color: whiteColor,
+                              ),
+                            ),
+
+                            title: "${data[index]['friend_name']}".text.fontFamily(semibold).color(darkFontGrey).make(),
+                            subtitle: "${data [index]['last_msg']}".text.make(),
+                          ),
+                        );
+                      },
+              ),
+                  )
+                ],
+              ),
+            );
           }
         },
       ),
