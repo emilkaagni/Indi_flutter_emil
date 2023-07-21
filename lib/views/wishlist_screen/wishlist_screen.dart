8988e66b5a64907c601596a9ea3fa1e68149cf7c
emilkaagni <basnyatemil02@gmail.com>
2023-07-21 22:30:03 +0545

    created page for wishlist

--- lib/views/wishlist_screen/wishlist_screen.dart
+++ lib/views/wishlist_screen/wishlist_screen.dart
@@ -26,7 +26,51 @@ class WishlistScreen extends StatelessWidget {
           } else if(snapshot.data!.docs.isEmpty){
             return "No orders yet!".text.color(darkFontGrey).makeCentered();
             }else{
-            return Container();
+            var data = snapshot.data!.docs;
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                      itemCount: data.length,
+                      itemBuilder: (BuildContext context, int index){
+                        return ListTile(
+                          leading: Image.network(
+                            "${data[index]['p_imgs'][0]}",
+                            width: 70,
+                            fit: BoxFit.cover,
+                          ),
+                          title: "${data[index]['p_name']}"
+                              .text
+                              .fontFamily(semibold)
+                              .size(16)
+                              .make(),
+                          subtitle: "${data[index]['p_price']}"
+                              .numCurrency
+                              .text
+                              .color(redColor)
+                              .fontFamily(semibold)
+                              .make(),
+
+                          trailing: const Icon(
+                            Icons.favorite,
+                            color: redColor,
+                          ).onTap(() async {
+                            await firestore
+                                .collection(productsCollection)
+                                .doc(data[index].id)
+                                .set({
+                              'p_wishlist': FieldValue.arrayRemove([currentUser!.uid])
+
+                            }, SetOptions(merge: true));
+                          }),
+
+                        );
+                      }
+                  ),
+                ),
+              ],
+            );
           }
         },
       ),
